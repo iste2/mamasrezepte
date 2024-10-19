1447c0a13acf9a9d509ee3c43cf69d3137fda86f
lnadenau <lukas.nadenau@inform-software.com>
2024-10-19 16:58:07 +0200

    styles

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppbarComponent } from './appbar/appbar.component';
+import {ToolbarModule} from 'primeng/toolbar';
+import {Button} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {MenubarModule} from 'primeng/menubar';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppbarComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToolbarModule,
+    Button,
+    InputTextModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
