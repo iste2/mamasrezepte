cc21dba3f9ff08ef851d2fef588baff7ef179c2f
lnadenau <lukas.nadenau@inform-software.com>
2024-10-19 23:02:42 +0200

    routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {RecipeListComponent} from './recipe-list/recipe-list.component';
+import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: RecipeListComponent, pathMatch: "full" },
+  { path: "recipes/:id", component: RecipeDetailComponent, pathMatch: "full" },
+  { path: "new-recipe", component: RecipeListComponent, pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
