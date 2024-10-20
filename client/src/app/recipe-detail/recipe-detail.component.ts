932cf34e2a0f8df51f291e22118a721a98ccaa21
lnadenau <lukas.nadenau@inform-software.com>
2024-10-20 10:47:49 +0200

    Rezept Detail

--- client/src/app/recipe-detail/recipe-detail.component.ts
+++ client/src/app/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../models/models';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RecipeService} from '../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,11 +12,16 @@ export class RecipeDetailComponent implements OnInit {
   recipe!: Recipe;
   id = "";
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService, private router: Router) {
 
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.id = this.route.snapshot.paramMap.get('id') ?? "";
+    this.recipe = await this.recipeService.getRecipe(this.id);
+  }
+
+  async editRecipe() {
+    await this.router.navigateByUrl(`/edit-recipe/${this.id}`);
   }
 }
